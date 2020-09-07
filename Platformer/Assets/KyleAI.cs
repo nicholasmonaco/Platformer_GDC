e6c3a9288f231bec5b2f6d6e06209dbc3751cc0b
Nick Monaco <nickjustpwnedu@gmail.com>
2020-09-07 00:11:28 -0500

    Initial commit

---
+++ Platformer/Assets/KyleAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyleAI : MonoBehaviour
+{
+    private Transform _playerTransform;
+    public Rigidbody2D EnemyRB;
+    public float Speed = 5;
+
+    private Vector2 _spawnPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        _spawnPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+
+    void Move() {
+        Vector2 dirToPlayer = _playerTransform.position - transform.position;
+        dirToPlayer.y = 0;
+        dirToPlayer.Normalize();
+
+        EnemyRB.velocity = new Vector2(dirToPlayer.x * Speed, EnemyRB.velocity.y);
+    }
+
+    public void ResetOnDeath() {
+        transform.position = _spawnPos;
+    }
+
+    public void KillEnemy() {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Platformer/Assets/PlayerController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public BoxCollider2D collider;
+    public float SpeedMultiplier = 5;
+    public float JumpForce = 10;
+
+    public bool Dead = false;
+    private Vector2 _spawnPos;
+
+    public int Lives = 3;
+
+    public TMP_Text LifeCounter;
+    public GameObject GameOverScreen;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnPos = transform.position;
+
+        LifeCounter.text = "Lives: " + Lives.ToString();
+        GameOverScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Lives < 0) {
+            //gameover
+            if (Input.anyKeyDown) {
+                //reset the game by reloading the scene
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                Time.timeScale = 1;
+            }
+        } else {
+            Move();
+            Jump();
+        }
+    }
+
+    void Move() {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if(horizontal != 0) {
+            rb.velocity = new Vector2(horizontal * SpeedMultiplier, rb.velocity.y);
+        }
+    }
+
+    void Jump() {
+        bool isGrounded = rb.velocity.y == 0;
+
+        Vector2 raycastOrigin = transform.position;
+        //raycastOrigin.y -= collider.size.y / 2;
+
+        LayerMask layerMask = LayerMask.GetMask("Default");
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin, Vector2.down, collider.size.y / 2 + 0.1f, layerMask);
+
+        if (hit.collider != null) {
+            Debug.Log("Hit ground: " + hit.collider.name);
+        }
+
+        if (isGrounded && Input.GetButton("Jump")) {
+            rb.AddForce(Vector2.up * JumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+    private void Die() {
+        Dead = true;
+
+        Lives--;
+
+        if(Lives < 0) {
+            //game over
+            GameOverScreen.SetActive(true);
+
+            Time.timeScale = 0;
+
+            return;
+        }
+
+        LifeCounter.text = "Lives: " + Lives.ToString();
+
+        GameObject[] ais = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach(GameObject enemy in ais) {
+            enemy.GetComponent<KyleAI>().ResetOnDeath();
+        }
+
+        transform.position = _spawnPos;
+
+        Dead = false;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.attachedRigidbody != null && collision.collider.attachedRigidbody.CompareTag("Enemy") && !Dead) {
+            if (transform.position.y - collider.size.y / 2 > collision.collider.transform.position.y + collision.collider.bounds.size.y / 2) {
+                collision.collider.attachedRigidbody.GetComponent<KyleAI>().KillEnemy();
+            } else {
+                Die();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("DeathPlane")) {
+            Die();
+        }
+    }
+}
